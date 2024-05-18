9fec3a05219359215664ea1381580c8bf7da5bce
Ion Fiat <ion.fiat@student.upt.ro>
2024-05-18 21:25:27 +0300

    Fisierele de la datele locale, avem o baza de date locala sub forma de fisiere si o accesam prin cale.

---
+++ dbo.MemberTbl.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[MemberTbl] (
+    [MId]     INT          IDENTITY (1, 1) NOT NULL,
+    [MName]   VARCHAR (50) NOT NULL,
+    [MPhone]  VARCHAR (50) NOT NULL,
+    [MGen]    VARCHAR (6)  NOT NULL,
+    [MAge]    INT          NOT NULL,
+    [MAmount] INT          NOT NULL,
+    [MTiming] VARCHAR (50) NOT NULL,
+    [MEmail] VARCHAR(50) NOT NULL, 
+    PRIMARY KEY CLUSTERED ([MId] ASC)
+);
+
